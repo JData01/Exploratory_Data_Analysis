946037bf8f1157224d4777456f31d037dd14bdef
J Data <j.datascience.01@gmail.com>
2023-01-30 14:34:20 -0500

    To print plot 2. version 1

---
+++ plot2.R
@@ -0,0 +1,40 @@
+library(dplyr)
+
+# LOADING THE DATA
+# “Individual household electric power consumption Data Set”
+# Date: Date in format dd/mm/yyyy
+# Time: time in format hh:mm:ss
+# Global_active_power: household global minute-averaged active power (in kilowatt)
+# Global_reactive_power: household global minute-averaged reactive power (in kilowatt)
+# Voltage: minute-averaged voltage (in volt)
+# Global_intensity: household global minute-averaged current intensity (in ampere)
+# Sub_metering_1: energy sub-metering No. 1 (in watt-hour of active energy). It corresponds to the kitchen, containing mainly a dishwasher, an oven and a microwave (hot plates are not electric but gas powered).
+# Sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It corresponds to the laundry room, containing a washing-machine, a tumble-drier, a refrigerator and a light.
+# Sub_metering_3: energy sub-metering No. 3 (in watt-hour of active energy). It corresponds to an electric water-heater and an air-conditioner.
+
+df <- data.frame(read.csv("household_power_consumption.txt",sep=";"))
+# info on the dataframe
+# head(df)
+# dim(df) #[1] 2075259    9   
+# make sure the columns are in the right format
+df <- mutate(df, Date= as.Date(Date, format="%d/%m/%Y"))
+# df <- mutate(df, Time= strptime(Time, format="%H:%M:%S")) #??????
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df$Global_intensity <- as.numeric(as.character(df$Global_intensity))
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+# str(df)
+
+# subset the dataframe between 2007-02-01 and 2007-02-02
+df_2007 <- df[df$Date >= "2007-02-01" & df$Date <= "2007-02-02", ]
+# head(df_2007)
+
+# MAKING PLOTS
+
+# PLOT 2
+df_2007["date.time"] <- as.POSIXct(paste(df_2007$Date, df_2007$Time), format="%Y-%m-%d %H:%M:%S")
+png(file="Plot2.png",width=480, height=480)
+plot(df_2007$date.time, df_2007.2$Global_active_power, type = "l", lty = 1, xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
